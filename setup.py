0d5dd461da284af58eef3931f48ddfa46d548d34
natemara <natemara@gmail.com>
2014-08-01 13:39:19 +0000

    added setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='python-erlang',
+    version='0.1',
+    description='Erlang calculation library for python',
+    url='http://github.com/natemara/python-erlang',
+    author='Nate Mara',
+    author_email='natemara@gmail.com',
+    license='MIT',
+    packages=['erlang'],
+    zip_safe=False
+)
