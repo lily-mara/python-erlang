32533008251290d73369fce2ef5af2806b1385ec
natemara <natemara@gmail.com>
2014-08-01 16:42:20 +0000

    added docstrings and improved readability

--- erlang/__init__.py
+++ erlang/__init__.py
@@ -1,27 +1,45 @@
-def extended_b_lines(bht, blocking):
+def extended_b_lines(usage, blocking):
+    '''
+    Uses the Extended Erlang B formula to calcluate the ideal number of lines
+    for the given usage in erlangs and the given blocking rate.
+    
+    Usage:
+    extended_b_lines(usage, blocking)
+    '''
+    
     line_count = 1
-    while extended_b(bht, line_count) > blocking:
+    while extended_b(usage, line_count) > blocking:
         line_count += 1
     return line_count
 
 
-def extended_b(traffic, lines, recall=0.5):
-    original_traffic = traffic
+def extended_b(usage, lines, recall=0.5):
+    '''
+    Usage:
+    extended_b(usage, lines, recall=0.5)
+    '''
+
+    original_usage = usage
     while True:
-        PB = b(traffic, lines)
-        magic_number_1 = (1 - PB) * traffic + (1 - recall) * PB * traffic
-        magic_number_2 = 0.9999 * original_traffic
+        PB = b(usage, lines)
+        magic_number_1 = (1 - PB) * usage + (1 - recall) * PB * usage
+        magic_number_2 = 0.9999 * original_usage
         if magic_number_1 >= magic_number_2:
             return PB
-        traffic = original_traffic + recall * PB * traffic
+        usage = original_usage + recall * PB * usage
     return -1
 
 
-def b(traffic, lines):
-    if traffic > 0:
-        PBR = (1 + traffic) / traffic
+def b(usage, lines):
+    '''
+    Usage:
+    b(usage, lines)
+    '''
+
+    if usage > 0:
+        PBR = (1 + usage) / usage
         for index in range(2, lines + 1):
-            PBR = index / traffic * PBR + 1
+            PBR = index / usage * PBR + 1
             if PBR > 10000:
                 return 0
         return 1 / PBR
