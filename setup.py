9084a635f2a2ec17f2e0855a41b236070377bc49
natemara <natemara@gmail.com>
2014-08-01 16:15:56 +0000

    updated setup for new version

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name='python-erlang',
-    version='0.1',
-    description='Erlang calculation library for python',
+    version='0.1.1',
+    description='Telecom calculation library for python',
     url='http://github.com/natemara/python-erlang',
     author='Nate Mara',
     author_email='natemara@gmail.com',
